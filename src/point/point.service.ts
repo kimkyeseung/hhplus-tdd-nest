bbeea4aef8a63a6ec96d785efddbbf59fe886369
Kim Kyeseung <chickenboys@naver.com>
2024-12-19 02:54:48 +0900

    update: 실페 케이스를 위한 예외처리 추가

--- src/point/point.service.ts
+++ src/point/point.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
 import { PointHistoryTable } from 'src/database/pointhistory.table';
 import { UserPointTable } from 'src/database/userpoint.table';
 import { PointHistory, TransactionType, UserPoint } from './point.model';
@@ -12,22 +16,42 @@ export class PointService {
   ) {}
 
   async findById(id: number): Promise<UserPoint> {
-    return await this.userDb.selectById(id);
+    const user = await this.userDb.selectById(id);
+    if (!user) {
+      throw new NotFoundException('사용자를 찾을 수 없습니다.');
+    }
+    return user;
   }
 
   async findHistoriesById(id: number): Promise<PointHistory[]> {
+    const user = await this.userDb.selectById(id);
+    if (!user) {
+      throw new NotFoundException('사용자를 찾을 수 없습니다.');
+    }
     return await this.historyDb.selectAllByUserId(id);
   }
 
   async getIsUsable(id: number, amount: number): Promise<boolean> {
-    const { point } = await this.userDb.selectById(id);
-
-    return point >= amount;
+    const user = await this.userDb.selectById(id);
+    if (!user) {
+      throw new NotFoundException('사용자를 찾을 수 없습니다.');
+    }
+    return user.point >= amount;
   }
 
   async charge(id: number, chargeDto: PointBody): Promise<PointHistory> {
-    const updateMillis = Date.now();
+    if (chargeDto.amount <= 0) {
+      throw new BadRequestException('충전 금액은 1원 이상의 정수여야 합니다.');
+    }
 
+    const user = await this.userDb.selectById(id);
+    if (!user) {
+      throw new NotFoundException('사용자를 찾을 수 없습니다.');
+    }
+
+    await this.userDb.insertOrUpdate(id, user.point + chargeDto.amount);
+
+    const updateMillis = Date.now();
     return await this.historyDb.insert(
       id,
       chargeDto.amount,
@@ -37,13 +61,22 @@ export class PointService {
   }
 
   async use(id: number, useDto: PointBody): Promise<PointHistory> {
-    const isUsable = this.getIsUsable(id, useDto.amount);
-    if (!isUsable) {
-      throw new Error('잔고가 충분하지 않습니다.');
+    if (useDto.amount <= 0) {
+      throw new BadRequestException('사용 금액은 1원 이상의 정수여야 합니다.');
     }
 
-    const updateMillis = Date.now();
+    const user = await this.userDb.selectById(id);
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
 
+    if (user.point < useDto.amount) {
+      throw new BadRequestException('잔고가 충분하지 않습니다.');
+    }
+
+    await this.userDb.insertOrUpdate(id, user.point - useDto.amount);
+
+    const updateMillis = Date.now();
     return await this.historyDb.insert(
       id,
       useDto.amount,
